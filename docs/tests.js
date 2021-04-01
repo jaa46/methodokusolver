cff516782c7cc3a5885a710b6a3dafb92b28ef91
Jonathan Agg <jonathan.agg@gmail.com>
2021-04-01 20:03:07 +0100

    Parse other format. Add more checkboxes.

--- docs/tests.js
+++ docs/tests.js
@@ -75,11 +75,46 @@ function addPuzzle(text, url) {
   for (var checkbox of checkboxes) {
     options[checkbox.id] = false;
   }  
-
+  
+  //TODO: make these visible to the user
+  var nums = ["numberOfHuntBells", "numberOfLeads", "consecutivePlaceLimit"];
+  for (var idx=0; idx<nums.length; idx++) {
+    options[nums[idx]] = -1;
+  }  
+  
   //Mark relevant options true
   var index = 0;
-  while(index < lines.length && lines[index][0] != "1") {
-    options[lines[index++].trim()] = true;
+  while(index < lines.length) {
+    if (bell2num(lines[index][0]) != "?")
+      break;
+    
+    var line = lines[index];
+    if(line.startsWith("##")) {
+      index++;
+      continue;
+    }
+    
+    if(line.startsWith("#")) {
+      var tokens = line.split(" ").filter(function(t) {return t.startsWith('-');});
+      tokens = tokens.map(function(t) {return parseToken(t);});
+      for(var idx=0; idx<tokens.length; idx++) {
+        var t = tokens[idx];
+        if (!Array.isArray(t))
+          t = [t];
+        for(var jdx=0; jdx<t.length; jdx++)
+          if(typeof(t[jdx])==="string" && t[jdx] in options)
+            options[t[jdx]] = true;
+          else if(t[jdx].name in options)
+            options[t[jdx].name] = t[jdx].value;
+          else
+            console.log("Unkown option: "+ t[jdx]);
+      }
+    }
+    else if (line.trim() in options) {
+      options[line.trim()] = true;
+    } 
+    
+    index++;
   }
     
   //Read puzzle
@@ -106,6 +141,106 @@ function addPuzzle(text, url) {
     numBells: numBells
   });
   
+}
+
+function parseToken(line) {
+  if (line.startsWith("-Q$o==")) {
+    //N lead plain course
+    numLeads = line.replace("-Q$o==", "");
+    line = {"name": 'numberOfLeads', "value": parseInt(numLeads)};
+  }
+else if (line.startsWith("-Q$B-$u==")) {
+    //TODO: Generalise this
+    //N working bells
+    numBells = line.replace("-Q$B-$u==", "");
+    console.log("Assuming " + line + " means N working bells")
+    line = "allWorkingExceptTreble";
+}
+else if (line.startsWith("-Q$u==")) {
+    //Number of hunt bells
+    numberOfHuntBells = line.replace("-Q$u==", "");
+    line = {"name": 'numberOfHuntBells', "value": parseInt(numberOfHuntBells)};
+}
+else if (line.startsWith("-m*(12|1")) {
+    //2 or N lead end
+    console.log("Assuming " + line + " means 2nds or Nths leadend")
+    line = "is2OrNLeadEnd";
+}
+else if (line.startsWith("-m*(1") && line != "-m*(12)") {
+    //N lead end
+    numBells = line.replace("-m*(1","")[0];
+    console.log("Assuming " + line + " means Nths leadend")
+    line = "isNLeadEnd";
+}
+else
+    switch (line.trim()) {
+        case "-p2":
+            line = "noLongPlaces";
+        break;
+        
+        case "--pbles":
+            line = "plainBobLeadEnd";
+        break;
+
+        case "-s":
+            line = "palindromicSymmetry";
+        break;
+            
+        case "-ds":
+            line = ["doubleSymmetry", "palindromicSymmetry"];
+        break;
+
+        case "-Fc":
+            line = "trueInLead"; // TODO: Make this trueInCourse
+        break;
+        
+        case "-m*(12)":
+            line = "is2LeadEnd";
+        break;
+
+        case "-T":
+            line = "trebleBob";
+        break;
+            
+        case "--delight":
+            line = "delight";
+        break;
+            
+        case "-S":
+            line = "surprise";
+        break;
+
+        case "-j":
+            line = {"name": 'consecutivePlaceLimit', "value": 0};
+        break;
+
+        case "-j2":
+            line = {"name": 'consecutivePlaceLimit', "value": 2};
+        break;
+
+        case "-l2":
+            line = "atMost2PlacesPerChange";
+        break;
+            
+        case "-w":
+            line = "rightPlace";
+        break;
+
+        case "-c":
+            line = "cyclicLeadEnd";
+        break;
+            
+        case "-Q$G==2&&$l~~\"1*\"" :
+            // Bells 2-N do the same work
+            line = ["fullCourse", "allWorkingExceptTreble"];
+        break;
+           
+        case "-Q$G==3&&$l~~\"12*\"" :
+            // Bells 3-N do the same work
+            line = ["fullCourse", "twoHuntBells"];
+        break;
+    }
+  return line;
 }
 
 var testResults = [];
