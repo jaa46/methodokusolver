2438e3bcc089e0feab0ae629c12bc08195314ebb
Jonathan Agg <jonathan.agg@gmail.com>
2021-04-29 18:50:12 +0100

    Handle principles in working bell logic. Fix bad assumption. Apply working bell logic at halflead if double symmetry.

--- docs/utilities.js
+++ docs/utilities.js
@@ -38,3 +38,12 @@ function integerRange(lowEnd, highEnd) {
   }
   return allOptions;
 }
+
+function isPrime(n) {
+ if (n%2==0) return (n==2);
+ var m = Math.sqrt(n);
+ for (var i=3;i<=m;i+=2) {
+  if (n%i==0) return false;
+ }
+ return true;
+}
